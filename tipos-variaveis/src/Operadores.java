076a96dd783d61409c0d63c3329be4242eb4ff43
RodriguesMarllon <marllonrsantos@gmail.com>
2024-04-25 12:04:08 -0300

    Operadores - 3

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,14 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero * -1;
+
+        System.out.println(numero);
     }
 }
